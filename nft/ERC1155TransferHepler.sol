f5cbd4211a6dcf9d7e65cc0b4c22c2095f4a1b9c
nullcanon <857149061@qq.com>
2022-07-18 21:44:22 +0800

    add

--- nft/ERC1155TransferHepler.sol
+++ nft/ERC1155TransferHepler.sol
@@ -13,6 +13,18 @@ contract BacthTransferHelper is Ownable{
         }
     }
 
+    function batchTransferWithNumber(address[] memory users, uint256[] memory numbers, uint256 start) public {
+        require(users.length == numbers.length, "length error");
+        for (uint256 n = 0; n < users.length; ++n) {
+            address user = users[n];
+            uint256 number = numbers[n];
+            for(uint256 i = start; i < start + number; i++) {
+                BeeItems(nftMintAddress).safeTransferFrom(msg.sender, user, i, 1, "");
+            }
+            start = start + number;
+        }
+    } 
+
     function setMint(address mint) public onlyOwner {
         nftMintAddress = mint;
     }
